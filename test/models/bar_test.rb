b949003ef559146390bb0403c6cae5fed428eb20
Trevor John <trevor@john.tj>
2017-12-01 08:57:15 -0500

    Add test to show bad group

--- test/models/bar_test.rb
+++ test/models/bar_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class BarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "grouping by name" do
+    foo = Foo.create!(name: 42)
+    Bar.create!(name: 'Fourty Two', foo: foo)
+
+    assert_equal Bar.joins(:foo).group('foos.name').count, { 42 => 1 }
+    assert_equal Foo.joins(:bars).group('bars.name').count, { 'Fourty Two' => 1 }
+  end
 end
